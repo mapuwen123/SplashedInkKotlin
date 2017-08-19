de5b1d82db64c22fe6a449e9eb8b26066d458c87
MaPuwen <mapuwen@outlook.com>
2017-08-19 15:19:43 +0800

    介绍图片

--- app/src/main/java/com/marvin/splashedinkkotlin/ui/download/DownloadActivity.kt
+++ app/src/main/java/com/marvin/splashedinkkotlin/ui/download/DownloadActivity.kt
@@ -6,6 +6,7 @@ import com.marvin.splashedinkkotlin.R
 import com.marvin.splashedinkkotlin.base.BaseActivity
 import com.marvin.splashedinkkotlin.bean.DiskDownloadBean
 import kotlinx.android.synthetic.main.activity_down_load.*
+import org.jetbrains.anko.doAsync
 
 class DownloadActivity : BaseActivity<DownloadView, DownloadPresenter>(), DownloadView {
     private val data: MutableList<DiskDownloadBean> = ArrayList()
